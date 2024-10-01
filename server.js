714ad7c4ac5153145a6545e67f1c0e3de84445f0
Amr Muhammad <133367131+Amr-Muhammad@users.noreply.github.com>
2024-10-01 03:13:24 +0300

    Webhook to handle success payment to add it to orders

--- server.js
+++ server.js
@@ -2,10 +2,61 @@ const express = require('express');
 const stripe = require('stripe')('sk_test_51PtX5uLbeudqBLeNEAc5erBrRhOADLqzmk0AeUYntUFjcXhhuwtszfstPJl9yr44yFbeYsNTcwJP1JbxseuHRgzG00hGNa0eJM');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
-app.use(bodyParser.json());
 app.use(cors());
+
+
+app.post('/webhook', express.raw({ type: 'application/json' }), async (req, res) => {
+  const sig = req.headers['stripe-signature'];
+
+  let event;
+
+  try {
+    event = stripe.webhooks.constructEvent(req.body, sig, 'whsec_cF8esG8EhA9L8nDRboCcZalbExcSsAhy');
+  } catch (err) {
+    return res.status(400).send(`Webhook Error: ${err.message}`);
+  }
+
+  if (event.type === 'checkout.session.completed') {
+    const session = event.data.object;
+
+    const retrievedSession = await stripe.checkout.sessions.retrieve(session.id, {
+      expand: ['line_items']
+    });
+
+    const userId = session.metadata.user_id;
+
+    let userCart = (await axios.get(`https://dailymart-5c550-default-rtdb.firebaseio.com/cart/${userId}.json`)).data
+
+    const orderData = {
+      items: userCart,
+      total: session.amount_total / 100,
+      status: 'Processing',
+      createdAt: new Date().toISOString(),
+      customerName: session.metadata.customer_name,
+      customerEmail: session.customer_email
+    };
+
+    try {
+      // Add the order to Firebase under the user's orders
+      const response = await axios.post(`https://dailymart-5c550-default-rtdb.firebaseio.com/orders/${userId}.json`, orderData);
+      if (response.status === 200) {
+        console.log('Order added successfully to Firebase');
+        await axios.delete(`https://dailymart-5c550-default-rtdb.firebaseio.com/cart/${userId}.json`);
+      } else {
+        console.error('Failed to add order to Firebase');
+      }
+    } catch (error) {
+      console.error('Error adding order to Firebase:', error);
+    }
+  }
+
+  res.json({ received: true });
+});
+
+app.use(bodyParser.json());
 
 app.post('/create-checkout-session', async (req, res) => {
   try {
@@ -50,7 +101,7 @@ app.post('/create-checkout-session', async (req, res) => {
       metadata: {
         customer_name: userName,
         user_id: userId,
-        cart: JSON.stringify(cartArray)
+        // cart: JSON.stringify(cartArray)
       }
     });
 
@@ -61,55 +112,6 @@ app.post('/create-checkout-session', async (req, res) => {
     res.status(500).json({ error: 'Internal Server Error' });
   }
 });
-
-
-app.post('/webhook', bodyParser.raw({ type: 'application/json' }), async (req, res) => {
-  const sig = req.headers['stripe-signature'];
-
-  let event;
-
-  try {
-    event = stripe.webhooks.constructEvent(req.body, sig, 'whsec_cF8esG8EhA9L8nDRboCcZalbExcSsAhy');
-  } catch (err) {
-    return res.status(400).send(`Webhook Error: ${err.message}`);
-  }
-
-  if (event.type === 'checkout.session.completed') {
-    const session = event.data.object;
-
-    const retrievedSession = await stripe.checkout.sessions.retrieve(session.id, {
-      expand: ['line_items']
-    });
-
-    const userId = session.metadata.user_id;
-    const cartArray = JSON.parse(session.metadata.cart);
-
-    const orderData = {
-      items: cartArray,
-      total: session.amount_total / 100,
-      status: 'processing',
-      createdAt: new Date().toISOString(),
-      customerName: session.metadata.customer_name,
-      customerEmail: session.customer_email
-    };
-
-    try {
-      // Add the order to Firebase under the user's orders
-      const response = await axios.post(`${FIREBASE_BASE_URL}/orders/${userId}.json`, orderData);
-
-      if (response.status === 200) {
-        console.log('Order added successfully to Firebase');
-      } else {
-        console.error('Failed to add order to Firebase');
-      }
-    } catch (error) {
-      console.error('Error adding order to Firebase:', error);
-    }
-  }
-
-  res.json({ received: true });
-});
-
 
 app.listen(3001, () => {
   console.log('Server running on port 3001');
